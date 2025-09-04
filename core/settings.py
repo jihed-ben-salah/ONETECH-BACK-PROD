46362b919e099e8a3d4fa6ac6a7517222d82d012
jihed-ben-salah <jihed.bensalah@esprit.tn>
2025-09-04 14:17:44 +0100

    add cors headers True

--- core/settings.py
+++ core/settings.py
@@ -79,5 +79,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+CORS_ALLOW_CREDENTIALS = True
 GEMINI_MODEL = os.getenv('GEMINI_MODEL', 'gemini-2.5-pro')
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
